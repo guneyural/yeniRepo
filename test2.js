93cc321d5346a4f77ebf95fbc4faef38bd1b5b78
guneyural <guneyural@gmail.com>
2024-02-14 11:58:01 +0300

    .gitignore eklendi

--- test2.js
+++ test2.js
@@ -1,6 +1,5 @@
-console.log('Test 2')
+console.log("Test 2");
 
-
-console.log('YENİ BİR SATIR DAHA EKLEDİM')
-console.log('Yepyeni bir satır daha')
-console.log('son bir satır daha')
+console.log("YENİ BİR SATIR DAHA EKLEDİM");
+console.log("Yepyeni bir satır daha");
+console.log("son bir satır daha");
